1bed49b61f4fafa43461c2444d4809708ebb9d1e
Elrapiz <rafael.pizarro.elrapiz@gmail.com>
2025-02-18 21:30:28 +0800

    The great repaktor

---
+++ src/pages/About.jsx
@@ -0,0 +1,20 @@
+import './../css/About.css';
+
+const About = () => {
+  return <>
+    <div className='container'>
+      <div>
+        <h1>I am Rafael Labrado Pizarro</h1>
+        <h2>A BSIT student from WMSU</h2>
+        <h3>Aspiring web developer</h3>
+        <h4>My stack is PERN</h4>
+      </div>
+
+      <div>
+        <p className='wave-icon'>👋</p>
+      </div>
+    </div>
+  </>;
+}
+
+export default About;
